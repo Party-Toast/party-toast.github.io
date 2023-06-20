cccb4586567a60847bd0bbff71c7b3736ef9ae6a
Walraven,Sytse S <s.walraven@student.fontys.nl>
2023-06-20 23:42:31 +0200

    Basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,70 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar app color="amber accent-4" dark elevation="10">
+            <v-app-bar-title>My Magazine</v-app-bar-title>
+            <template v-slot:extension>
+                <v-tabs 
+                    v-model="tab" 
+                    grow
+                    center-active
+                >
+                    <v-tab v-for="(item, index) in items" :key="index">
+                        <v-icon class="mr-2">{{ item.icon }}</v-icon>
+                        {{ item.title }}
+                    </v-tab>
+                </v-tabs>
+            </template>
+        </v-app-bar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+        <v-main>
+            <v-container fluid>
+                <v-tabs-items v-model="tab">
+                    <v-tab-item
+                        v-for="(item, index) in items" :key="index"
+                    >
+                        <v-card class="ma-3"> 
+                            <v-card-title>{{ item.title }}</v-card-title>
+                            <v-card-text>{{ item.text }}</v-card-text>
+                        </v-card>
+                    </v-tab-item>
+                    </v-tabs-items>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+    components: {
+    },
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        tab: null,
+        items: [
+            {
+                title: 'Exploring Passion and Purpose',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.',
+                icon: 'mdi-heart',
+            },
+            {
+                title: 'Creating Impact',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.',
+                icon: 'mdi-rocket',
+            },
+            {
+                title: 'Developing Knowledge and Skills',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.',
+                icon: 'mdi-flask',
+            },
+            {
+                title: 'Creating a Future Plan',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl. Nulla euismod, nisl vitae aliquam ultricies, nunc nisl ultricies nunc, vitae aliquam nisl nisl vitae nisl.',
+                icon: 'mdi-calendar',
+            }
+        ],
+    }),
 };
 </script>
